e6f0c8f162f756862b5410a4a98f27375697eedd
Brian <brigeveriz7@gmail.com>
2023-08-30 00:16:25 +0300

    Set up onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -6,7 +6,7 @@ async function Page(){
     const userInfo = {}
     const userData = {
         id: user?.id,
-        objectId: userInfo?.id,
+        objectId: userInfo?._id,
         username: userInfo?.username || user?.username,
         name: userInfo?.name || user?.firstname || "",
         bio: userInfo?.bio || "",
